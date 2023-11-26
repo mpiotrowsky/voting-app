5bbfadf1a69ee7dc9f64e26692fa836bd08a18ae
Mateusz Piotrowski <mpiotrov@gmail.com>
2023-11-26 18:10:57 +0100

    Clean material module imports

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,58 +1,32 @@
 import { NgModule } from '@angular/core';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatTableModule } from '@angular/material/table';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule( {
     imports: [
         MatButtonModule,
-        MatMenuModule,
-        MatToolbarModule,
         MatIconModule,
-        MatCardModule,
         MatInputModule,
         MatFormFieldModule,
-        MatSidenavModule,
-        MatExpansionModule,
-        MatCheckboxModule,
         MatSelectModule,
         MatDialogModule,
-        MatTableModule,
         MatSnackBarModule,
-        MatProgressBarModule,
     ],
     exports: [
         MatButtonModule,
-        MatMenuModule,
-        MatToolbarModule,
         MatIconModule,
-        MatCardModule,
         MatInputModule,
         MatFormFieldModule,
-        MatSidenavModule,
-        MatExpansionModule,
-        MatCheckboxModule,
         MatSelectModule,
         MatDialogModule,
-        MatTableModule,
         MatSnackBarModule,
-        MatProgressBarModule,
     ],
     providers: [
-        MatDatepickerModule,
     ]
 } )
 
