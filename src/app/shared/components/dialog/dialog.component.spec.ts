013d969c40af69ac8b0d51825830ba12b1227eac
Mateusz Piotrowski <mpiotrov@gmail.com>
2023-11-27 15:19:59 +0100

    Add basic test for dialog component

--- src/app/shared/components/dialog/dialog.component.spec.ts
+++ src/app/shared/components/dialog/dialog.component.spec.ts
@@ -1,23 +1,64 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync, tick, fakeAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { DialogComponent } from './dialog.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 
 describe('DialogComponent', () => {
   let component: DialogComponent;
   let fixture: ComponentFixture<DialogComponent>;
+  let matDialogRefMock: MatDialogRef<DialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DialogComponent]
+  beforeEach(waitForAsync(() => {
+    matDialogRefMock = jasmine.createSpyObj('MatDialogRef', ['close']);
+
+    TestBed.configureTestingModule({
+      imports: [DialogComponent, FormsModule, MatButtonModule, MatDialogModule, MatFormFieldModule, MatInputModule, NoopAnimationsModule],
+      providers: [
+        { provide: MatDialogRef, useValue: matDialogRefMock },
+        { provide: MAT_DIALOG_DATA, useValue: { title: 'Test' } }
+      ]
     })
     .compileComponents();
-    
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(DialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should close the dialog with false when Cancel button is clicked', () => {
+    const cancelButton = fixture.nativeElement.querySelector('#btn-cancel');
+    cancelButton.click();
+
+    expect(matDialogRefMock.close).toHaveBeenCalledWith(false);
+  });
+
+  it('should close the dialog with the entered name when Add button is clicked', () => {
+    component.name = 'Johny Bravo';
+    fixture.detectChanges();
+
+    const addButton = fixture.nativeElement.querySelector('#btn-add');
+    addButton.click();
+
+    expect(matDialogRefMock.close).toHaveBeenCalledWith({ name: 'Johny Bravo' });
+  });
+
+  it('should disable the Add button when name is empty', () => {
+    const addButton = fixture.nativeElement.querySelector('#btn-add');
+    expect(addButton.disabled).toBeTruthy();
+
+    component.name = 'Johny Bravo';
+    fixture.detectChanges();
+
+    expect(addButton.disabled).toBeFalsy();
   });
 });
